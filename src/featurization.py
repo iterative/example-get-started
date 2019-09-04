72e0f12cb2eca10f557c846bc706d0a5a321c9f3
Jorge Orpinel <jorge@orpinel.com>
2019-09-03 19:20:41 -0500

    Reproduce model using bigrams

--- src/featurization.py
+++ src/featurization.py
@@ -75,7 +75,7 @@ df_train = get_df(train_input)
 train_words = np.array(df_train.text.str.lower().values.astype('U'))
 
 bag_of_words = CountVectorizer(stop_words='english',
-                               max_features=5000)
+                               max_features=6000, ngram_range=(1, 2))
 bag_of_words.fit(train_words)
 train_words_binary_matrix = bag_of_words.transform(train_words)
 tfidf = TfidfTransformer(smooth_idf=False)
